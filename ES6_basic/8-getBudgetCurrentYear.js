60468286db68dc8f303d183fa64ad0b460838db2
butterfly930 <elsarrustaj@gmail.com>
2024-07-09 00:01:57 +0200

    commit

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,13 +4,10 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const currentYear = getCurrentYear();
-  
   const budget = {
-  [`income-${getCurrentYear()}`]: income,
-  [`gdp-${getCurrentYear()}`]: gdp,
-  [`capita-${getCurrentYear()}`]: capita,
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
-
   return budget;
 }
