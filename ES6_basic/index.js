8d3fbca4b3836c01cde63e4cba8970a1ea28a493
butterfly930 <elsarrustaj@gmail.com>
2024-07-08 12:51:25 +0200

    2-arrow

---
+++ ES6_basic/index.js
@@ -0,0 +1,30 @@
+const today = "Thursday";
+
+const message = "Today is :";
+
+console.log(" i just wont to say:" + message + " " + today);
+
+console.log(`i just wont to say: ${message} ${today} `);
+
+--------------------------------------------------------------------------------
+
+const myName = null ;
+console.log(myName);
+
+-------------------------------------------------------------------------------
+
+const myName = undefined;
+console.log(myName);
+
+-------------------------------------------------------------------------------
+
+console.log(sayHello());
+function sayHello() {
+    return"Hello";
+}
+
+//vlen e njejta gje edhe kur e deklaron me let 
+//
+textt = "strrr"
+console.log(textt);
+var textt;
