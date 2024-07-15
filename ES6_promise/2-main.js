6d1d54c12522a272de4784977ddaae8b59c04ad0
butterfly930 <elsarrustaj@gmail.com>
2024-07-15 14:17:52 +0200

    commit excercise 2-then.js

--- ES6_promise/2-main.js
+++ ES6_promise/2-main.js
@@ -2,3 +2,6 @@ import handleResponseFromAPI from "./2-then";
 
 const promise = Promise.resolve();
 handleResponseFromAPI(promise);
+
+const promise2 = Promise.reject();
+handleResponseFromAPI(promise2);
