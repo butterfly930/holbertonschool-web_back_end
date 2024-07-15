7ab4108bbb8aab94573ed6a80a590dd37f4e323c
butterfly930 <elsarrustaj@gmail.com>
2024-07-15 14:10:08 +0200

    commit excercise 1-promise.js

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -6,6 +6,6 @@ export default function getFullResponseFromAPI(success) {
         else {
             reject(new Error("The fake API is not working currently"));
         }
-        
+                
 });
 }
