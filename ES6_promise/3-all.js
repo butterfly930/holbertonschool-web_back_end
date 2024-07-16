2dbaefe98f293e594ef9bec463596e7c8b2a30ee
butterfly930 <elsarrustaj@gmail.com>
2024-07-16 10:40:17 +0200

    update 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,11 @@
-import { uploadPhoto, createUser } from './utils';
-
+import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
-    })
-    .catch(() => console.log('Signup system offline'));
+  .then(([photo, user]) =>{
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  })
+  .catch(() => {
+    console.log ('Signup system offline');
+  });
 }
+import { uploadPhoto, createUser } from './utils';
