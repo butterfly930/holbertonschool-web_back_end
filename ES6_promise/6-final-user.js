d1f59e5b41adff8a0823ed0af34237b152ec44a7
butterfly930 <elsarrustaj@gmail.com>
2024-07-16 18:44:49 +0200

    changes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
     const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-   const results = await Promise.allSettled(promises);
-  console.log(results);
+   return Promise.allSettled(promises).then((results)=>{
+   
+ const data = [
+    {
+    status: 'fulfilled',
+    value: { firstName: 'Bob', lastName: 'Dylan' }
+  },
+  {
+    status: 'rejected',
+     Error: "bob_dylan.jpg cannot be processed"
+   }
+]
+   console.log(data);
+   });
 };
