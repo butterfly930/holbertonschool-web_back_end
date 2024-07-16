b2282c0f7aab493bd3f12be384de138e93b45d3a
butterfly930 <elsarrustaj@gmail.com>
2024-07-16 18:50:50 +0200

    changes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,21 +1,24 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
+/*eslint-disable*/
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-   return Promise.allSettled(promises).then((results)=>{
-   
- const data = [
-    {
-    status: 'fulfilled',
-    value: { firstName: 'Bob', lastName: 'Dylan' }
-  },
-  {
-    status: 'rejected',
-     Error: "bob_dylan.jpg cannot be processed"
-   }
-]
-   console.log(data);
-   });
-};
+  const userPromise = signUpUser(firstName, lastName)
+    .then((value) => ({
+      status: "fulfilled",
+      value,
+    }))
+    .catch((error) => ({
+      status: "rejected",
+      value: error.toString(),
+    }));
+  const photoPromise = uploadPhoto(fileName)
+    .then((value) => ({
+      status: "fulfilled",
+      value,
+    }))
+    .catch((error) => ({
+      status: "rejected",
+      value: error.toString(),
+    }));
+  return Promise.all([userPromise, photoPromise]);
+}
