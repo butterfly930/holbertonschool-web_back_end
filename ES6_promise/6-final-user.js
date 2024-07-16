4379de7575e333643e2b53c166dde9f9677e0a25
butterfly930 <elsarrustaj@gmail.com>
2024-07-16 18:30:46 +0200

    changes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
     const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-   return Promise.allSettled(promises).then((results)=>{
-    console.log(results);
-   });
+   const results = await Promise.allSettled(promises);
+  console.log(results);
 };
