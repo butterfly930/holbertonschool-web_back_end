93956001d3aebb03b066b91f4668a48e8a3ae677
butterfly930 <elsarrustaj@gmail.com>
2024-07-16 11:18:50 +0200

    3-all recommit

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
   .then(([photo, user]) =>{
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  })
-  .catch(() => {
+  }).catch(() => {
     console.log ('Signup system offline');
   });
 }
