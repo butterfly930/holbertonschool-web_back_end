f88f664e1bcc5eeb3b0e8899644fdcc00863f9b2
butterfly930 <elsarrustaj@gmail.com>
2024-07-16 17:37:57 +0200

    update

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,9 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 const handleProfileSignup = async (firstName, lastName, fileName) => {
   try {
-    const user = await signUpUser(firstName, lastName);
-    await uploadPhoto(fileName);
-    console.log(`${user.firstName} ${user.lastName}`);
+   return Promise.all([signUpUser(firstName, lastName),uploadPhoto(fileName)]);
   } catch (error) {
     console.log(error.message);
   }
