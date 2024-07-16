bb33f2588509b5419863a4ac224f74d1979fe9f5
butterfly930 <elsarrustaj@gmail.com>
2024-07-16 17:01:06 +0200

    update

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,8 +1,8 @@
-export default function getFullResponseFromAPI (success) {
+export default functiongetFullResponseFromAPI (success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
-    }else {
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
