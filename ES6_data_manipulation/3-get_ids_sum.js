9c6070028feafe992d0cc24090ea66294b1843c1
butterfly930 <elsarrustaj@gmail.com>
2024-07-26 21:31:21 +0200

    commit ecs 3

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,13 @@
-export default function getStudentIdsSum(listOfStudents) {
-    return listOfStudents.reduce((sum, student) => sum + student.id, 0);
+function getStudentIdsSum(students) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+
+    const reducer = (accumulator, student) => accumulator + student.id;
+
+    const sum = students.reduce(reducer, 0);
+
+    return sum;
 }
+
+export default getStudentIdsSum;
