974a395541de1ee986e27624703754743725b7fe
butterfly930 <elsarrustaj@gmail.com>
2024-07-26 21:14:02 +0200

    commit

--- ES6_data_manipulation/1-get_list_student_ids.js
+++
@@ -1,6 +0,0 @@
-export default function getListStudentIds(arrayOfObjects) {
-    if (!Array.isArray(arrayOfObjects)) {
-        return [];
-    }
-    return arrayOfObjects.map((student) => student.id);
-}
