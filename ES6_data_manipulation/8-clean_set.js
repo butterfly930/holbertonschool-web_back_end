36034b989bc7e7e7c6cc97c08bcdb955763617c9
butterfly930 <elsarrustaj@gmail.com>
2024-07-26 21:46:09 +0200

    exercise 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-    const arr = [];
-    if (typeof startString === 'undefined' || startString === '' || typeof startString !== 'string') {
-        return '';
+  const arr = [];
+  if (typeof startString === 'undefined' || startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+  set.forEach((element) => {
+    if (typeof element !== 'undefined') {
+      if (element.startsWith(startString)) {
+        arr.push(element.split(startString)[1]);
+      }
     }
-    set.forEach((element) => {
-        if (typeof element !== 'undefined') {
-            if (element.startsWith(startString)) {
-                arr.push(element.split(startString)[1]);
-            }
-        }
-    });
-    return arr.join('-');
+  });
+  return arr.join('-');
 }
