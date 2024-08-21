ffc390699b2346ef2d579c4c0d50a394dc2d434f
butterfly930 <elsarrustaj@gmail.com>
2024-08-21 14:05:05 +0200

    first commit

---
+++ MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+README for directory MySQL_Advanced
---
+++ MySQL_Advanced/queries.sql
@@ -0,0 +1,8 @@
+USE my_database;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
+
+
