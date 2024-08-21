046b2c403ced7db6ac4d3c5fddfee5943ef715fe
butterfly930 <elsarrustaj@gmail.com>
2024-08-21 14:29:03 +0200

    exc 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,8 @@
 -- Make a table
 -- If table exist, you should not fail
 CREATE TABLE IF NOT EXISTS users (
-  id int  AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  name VARCHAR(255)
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
 );
-
-INSERT INTO users (email, name) VALUES ('bob@dylan.com', 'Bob');
-INSERT INTO users (email, name) VALUES ('sylvie@dylan.com', 'Sylvie');
