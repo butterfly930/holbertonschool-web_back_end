da257d62673e64514672a256bcad1206fe654a60
butterfly930 <elsarrustaj@gmail.com>
2024-08-21 14:59:11 +0200

    exc 2

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
 -- Make a table
--- If table exist, you should not fail
-CREATE TABLE IF NOT EXISTS users (
+-- If table exists, it should not fail
+CREATE TABLE IF NOT EXISTS users1 (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY(id)
 );
