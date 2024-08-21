5b0edfe6c8b9ea70af4fd3480737a55d7d788864
butterfly930 <elsarrustaj@gmail.com>
2024-08-21 15:00:34 +0200

    2nd exc

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- Make a table
 -- If table exists, it should not fail
-CREATE TABLE IF NOT EXISTS users1 (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
